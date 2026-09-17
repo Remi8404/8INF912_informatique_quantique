ed0f55485fd365a6ba203901e8512e21924786a9
REMI AERTS <remi.aerts@etu.utc.fr>
2026-09-17 15:32:31 -0400

    updated requirements and q5

--- dev_3.py
+++ dev_3.py
@@ -3,9 +3,10 @@ from qiskit.quantum_info import Statevector
 from consts import H, PLUS, MINUS, X, Y, Z
 
 def main():
-    print(f"Q1.a)\n{H ^ X}")
+    print(f"Q1.a)\n{H ^ X}") # same as H.tensor(X)
     print(f"Q1.b)\n{X ^ Y ^ Z}")
     print(f"Q1.c)\n{Statevector(PLUS) ^ Statevector(MINUS)}")
+    print(f"Q5)\n{Statevector(MINUS) ^ Statevector(MINUS)}")
     
 if __name__ == "__main__":
     main()
