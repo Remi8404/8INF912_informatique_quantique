f6c25e4eeeec368c1f29cfd292d5df774cea3c39
Kelian banco <bancokelian@gmail.com>
2026-09-26 10:26:56 -0400

    test de l'addtionneur

--- dev_4.py
+++ dev_4.py
@@ -1,21 +1,21 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit.visualization import plot_histogram
-#from qiskit_aer import AerSimulator
+from qiskit_aer import AerSimulator
 from qiskit import transpile
 
 
 #maj 
-qc_maj = QuantumCircuit(3, 1)
+qc_maj = QuantumCircuit(3)
 qc_maj.cx(2,1)
 qc_maj.cx(2,0)
 qc_maj.ccx(0,1,2)
 
 
 #uma
-qc_uma = QuantumCircuit(3, 1)
+qc_uma = QuantumCircuit(3)
 qc_uma.ccx(0,1,2)
 qc_uma.cx(2,0)
-qc_uma.cx(2,1)
+qc_uma.cx(0,1)
 
 print(qc_maj.draw(output='text'))
 print(qc_uma.draw(output='text'))
@@ -57,3 +57,18 @@ qc.append(porte_uma, [c0[0], b[0], a[0]])
 qc.barrier()
 
 #----------------------------------------------------------------------------
+
+print(qc.draw(output='text'))
+
+qc.measure(b, c_reg[0:4]) #résultat de la somme
+qc.measure(cout, c_reg[4])
+
+
+#----------------------------------------------------------------------------
+aer = AerSimulator()
+compile = transpile(qc, aer)
+job = aer.run(compile, shots=1000)
+result = job.result()
+counts = result.get_counts(qc)
+
+print(counts)
