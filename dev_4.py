f79407b96425dfb38a3675046660229a32b578d2
Kelian banco <bancokelian@gmail.com>
2026-09-26 10:07:39 -0400

    debut dev_

---
+++ dev_4.py
@@ -0,0 +1,59 @@
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit.visualization import plot_histogram
+#from qiskit_aer import AerSimulator
+from qiskit import transpile
+
+
+#maj 
+qc_maj = QuantumCircuit(3, 1)
+qc_maj.cx(2,1)
+qc_maj.cx(2,0)
+qc_maj.ccx(0,1,2)
+
+
+#uma
+qc_uma = QuantumCircuit(3, 1)
+qc_uma.ccx(0,1,2)
+qc_uma.cx(2,0)
+qc_uma.cx(2,1)
+
+print(qc_maj.draw(output='text'))
+print(qc_uma.draw(output='text'))
+
+porte_maj = qc_maj.to_gate()
+porte_uma = qc_uma.to_gate()
+
+#----------------------------------------------------------------------------
+c0 = QuantumRegister(1, 'c0')
+a = QuantumRegister(4, 'a')
+b = QuantumRegister(4, 'b')
+cout = QuantumRegister(1, 'cout')
+c_reg = ClassicalRegister(5, 'mesure')
+
+qc = QuantumCircuit(c0, a, b, cout, c_reg)
+
+qc.x(a[0])
+qc.x(a[2])
+
+qc.x(b[0])
+qc.x(b[1])
+qc.x(b[3])
+
+qc.barrier()
+
+#----------------------------------------------------------------------------
+qc.append(porte_maj, [c0[0], b[0], a[0]])
+qc.append(porte_maj, [a[0], b[1], a[1]])
+qc.append(porte_maj, [a[1], b[2], a[2]])
+qc.append(porte_maj, [a[2], b[3], a[3]])
+
+qc.cx(a[3], cout[0])
+
+qc.append(porte_uma, [a[2], b[3], a[3]])
+qc.append(porte_uma, [a[1], b[2], a[2]])
+qc.append(porte_uma, [a[0], b[1], a[1]])
+qc.append(porte_uma, [c0[0], b[0], a[0]])
+
+qc.barrier()
+
+#----------------------------------------------------------------------------
